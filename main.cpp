b20b831471a2748826064aba03d6a1a236a3131a
Zx <148573313+zx-0b111@users.noreply.github.com>
2025-07-12 04:56:54 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,49 +1,65 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <random>
 using namespace std;
 
 string password = "";
 
 void Letters() {
-	string letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	random_device seed;
+	mt19937 random(seed());
 	int valor;
 	cout << "Quantity letters: ";
 	cin >> valor;
+	uniform_int_distribution<> distribution(0, valor);
+	
+	string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	
 	for (int i = 0; i < valor; i++) {
-		int index = rand() % letters.length(); //calculo para garantir que o numero gerado esteja dentro dos limites da variavel.
+		int index = distribution(random);
 		password += letters[index];
 	}
 }
 
 void numbers() {
-	string numbers = "12345678910";
+	
+	//Random engine using Mersenne Twister
+	random_device seed; //Obtain a seed from the hardware
+	mt19937 random(seed()); //Initialize Mersenne Twister with seed
 	int valor2;
 	cout << "Quantity numbers: ";
 	cin >> valor2;
+	
+	//Define a uniform integer distribution
+	uniform_int_distribution<> distribution(0, valor2); //Generate a number 
 
+	string numbers = "1234567890";
+	
 	for (int i = 0; i < valor2; i++) {
-		int index2 = rand() % numbers.length();
+		int index2 = distribution(random); //Get a random index
 		password+= numbers[index2];
 	}
 }
 
 void characters() {
-	string character = "@-_#!$%¨&*()[]{}";
+	random_device seed;
+	mt19937 random(seed());
 	int valor3;
 	cout << "Quantity characters: ";
 	cin >> valor3;
+	uniform_int_distribution<> distribution(0, valor3);
+	
+	string character = "@-_#!$%¨&*()[]{}";
 
 	for (int i = 0; i < valor3; i++) {
-		int index3 = rand() % character.length(); //números aleatórios gerados em um intervalo de 0 a 15.
+		int index3 = distribution(random);
 		password+=character[index3];
 	}
 }
 
 int main()
 {
-	srand(time(0));
 	Letters();
 	numbers();
 	characters();
